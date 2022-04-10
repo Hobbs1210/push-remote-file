950959ec8ac9acd7a7f2b2c0768c7921b1339237
Habib Zaid <habibzaid@gmail.com>
2022-04-10 13:05:02 -0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+url="$1"
+file_in_repo="$2"
+git_name="$3"
+git_email="$4"
+git_branch="$5"
+
+mkdir -p "$(dirname "$file_in_repo")"
+wget "$url" -O "$file_in_repo"
+
+git config user.name "$git_name"
+git config user.email "$git_email"
+
+git add "$file_in_repo"
+git commit -m "Update $(basename "$file_in_repo") from $url"
+git push origin "$git_branch"
