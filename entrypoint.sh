22923b96908ca198c65a9ee1924655c51113046e
Habib Zaid <habibzaid@gmail.com>
2022-04-11 07:02:42 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,17 +16,24 @@ commit_message="${12}"
 mkdir -p "$(dirname "$file_in_repo")";
 wget "$url" -O "$file_in_repo";
 
+echo "REPOSITORY value: $repository";
 cd "$repository";
 
+echo "BRANCH value: $branch";
 git checkout $branch --;
 
+echo "::debug::Apply add options ${add_options}";
 git add ${add_options} ${file_pattern};
 
 git config user.name "$commit_user_name";
 git config user.email "$commit_user_email";
 
+echo "::debug::Apply commit options ${commit_options}";
 commit_options_array=( $commit_options );
 git commit -m "$commit_message" --author="$commit_author" ${commit_options:+"${commit_options_array[@]}"};
 
 push_options_array = ( $push_options );
+
+echo "::debug::Apply push options ${push_options}";
+echo "::debug::git push origin";
 git push origin ${push_options:+"${push_options_array[@]}"};
