d08ff7dbbbdd4176c949d57764675770a2d6216d
Habib Zaid <habibzaid@gmail.com>
2022-04-11 06:03:42 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,29 @@
 
 url="$1"
 file_in_repo="$2"
-branch="$3"
-commit_options="$4"
-add_options="$5"
-file_pattern="$6"
-push_options="$7"
-commit_user_name ="$8"
-commit_user_email ="$9"
-commit_author="$10"
+repository="$3"
+branch="$4"
+commit_options="$5"
+add_options="$6"
+file_pattern="$7"
+push_options="$8"
+commit_user_name ="$9"
+commit_user_email ="$10"
+commit_author="$11"
+commit_message="$12"
 
 mkdir -p "$(dirname "$file_in_repo")"
 wget "$url" -O "$file_in_repo"
+
+cd "$repository"
 
 git checkout "$branch" --
 
 git add "$add_options" "$file_pattern"
 
-git config user.name "$commit_user_name "
-git config user.email "$commit_user_email "
+git config user.name "$commit_user_name"
+git config user.email "$commit_user_email"
 
-git commit -m "Update $(basename "$file_in_repo") from $url"  --author="$commit_author" "$commit_options"
+git commit -m "$commit_message" --author="$commit_author" "$commit_options"
 
 git push origin "$push_options"
